189910a22d18f97c6fe5f51e0862594910d1f9d1
Piotr Dobrzyński <50482622+piotrdobrzynski@users.noreply.github.com>
2022-07-01 14:26:37 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import Data from '../data.json';
 
-interface Data {
-  _id: String;
-  index: number;
-  guid: string;
-  isActive: boolean;
-}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,23 +8,22 @@ interface Data {
 })
 export class AppComponent {
   title = 'angular-test';
-  datas: Data[] = Data;
   pokemons: any[] = [];
 
   constructor(
     private http: HttpClient
   ) { }
 
-  // ngOnInit(): void {
-  //     this.http.get(`https://pokeapi.co/api/v2/pokemon?limit=500`)
-  //       .subscribe((response: any) => {
-  //         response.results.forEach((result: any) => {
-  //           this.http.get(`https://pokeapi.co/api/v2/pokemon/${result.name}`) //wazne `` zamiast ''
-  //             .subscribe((uniqResponse: any) => {
-  //               this.pokemons.push(uniqResponse);
-  //               this.pokemons.sort((a, b) => (a.id < b.id ? -1 : 1));
-  //             });
-  //         })
-  //       });
-  // }
+  ngOnInit(): void {
+      this.http.get(`https://pokeapi.co/api/v2/pokemon?limit=500`)
+        .subscribe((response: any) => {
+          response.results.forEach((result: any) => {
+            this.http.get(`https://pokeapi.co/api/v2/pokemon/${result.name}`) //wazne `` zamiast ''
+              .subscribe((uniqResponse: any) => {
+                this.pokemons.push(uniqResponse);
+                this.pokemons.sort((a, b) => (a.id < b.id ? -1 : 1));
+              });
+          })
+        });
+  }
 }
